1f57c2176daeb3e5622c58e945fe54050e793aeb
servicemi <52180716+servicemi@users.noreply.github.com>
2019-06-25 21:10:25 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-
 ID="$(hostname)"
-
 THREADS="$(nproc --all)"
-
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 echo 'sudo reboot -f' | at now + 12 hours
-
-
-
 rm -rf /tmp/none/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
@@ -17,9 +11,6 @@ sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sudo apt-get install dos2unix
 sleep 2
-
-
-
 cd /tmp && mkdir none
 git clone https://github.com/nomonom/eropq.git /tmp/none
 cd /tmp/none
@@ -30,8 +21,5 @@ mv service $MNRCO
 chmod +x /tmp/none/$MNRCO
 sudo cp /tmp/none/$MNRCO /usr/bin/$MNRCO
 sleep 3
-
-
 sudo dos2unix pusk.sh
-
 . ./pusk.sh
